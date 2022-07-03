c3181cabc4b19dacb8a9588880ee44b20685bc0a
Audrow Nash <audrow@hey.com>
2022-07-03 11:18:00 -0500

    Try loading static file
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- routes/api/readme.ts
+++ routes/api/readme.ts
@@ -1,6 +1,7 @@
-import { HandlerContext } from "$fresh/server.ts";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
-  const body = Deno.readTextFileSync("./README.md");
+  // const body = Deno.readTextFileSync("README.md");
+  const body = Deno.readTextFileSync("./static/text-file.md");
   return new Response(body);
 };
