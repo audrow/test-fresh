312272612f6faf35f9fe2763088ee7602ae272fc
Audrow Nash <audrow@hey.com>
2022-07-04 11:11:43 -0500

    Update routes
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,8 @@
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/api/readme.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $3 from "./routes/github/[username].tsx";
+import * as $4 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -13,7 +14,8 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/api/readme.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/github/[username].tsx": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
